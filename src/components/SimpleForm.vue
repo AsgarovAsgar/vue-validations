1b974d1723ebdde700c402315ac9b2ede5d47127
Asgarov Asgar <asgara@maestro.st>
2023-11-21 23:59:17 +0400

    added simple form

--- src/components/SimpleForm.vue
+++ src/components/SimpleForm.vue
@@ -1,5 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+
+const form = ref({
+  name: '',
+  age: ''
+})
+
+function submitForm() {
+  const nameIsValid = !!form.value.name
+  const ageIsValid = !!form.value.age
+
+  const formIsValid = nameIsValid && ageIsValid
+  if(formIsValid) {
+    console.log('Submitted', form.value);
+  } else {
+    console.log('invalid form');
+  }
+}
+
+</script>
+
 <template>
   <div>
-    fgg
+    <form @submit.prevent="submitForm" class="mt-4 p-4 border inline-flex flex-col gap-4">
+      <div class="flex flex-col">
+        <label for="name">Name:</label>
+        <input type="text" v-model="form.name" id="name">
+      </div>
+      
+      <div>
+        <label for="age">Age:</label>
+        <input type="text" v-model="form.age" id="age">
+      </div>
+
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
